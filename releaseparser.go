b15f54f08b26b0b68426d931a534a9c7e3644944
cytec <iamcytec@googlemail.com>
2017-10-22 23:28:36 +0200

    refine regexes and add group names for video and codec

--- releaseparser.go
+++ releaseparser.go
@@ -10,55 +10,59 @@ var (
 	season     = `([Ss]?([0-9]{1,2}))[Eex]|([Ss]([0-9]{1,2}))`
 	episode    = `([Eex]([0-9]{2,4}(?:[abc])?)(?:[^0-9]|$))`
 	year       = `([\[\(]?((?:19[0-9]|20[01])[0-9])[\]\)]?)`
-	resolution = `(([0-9]{3,4}(?:p|i)))[^M]`
-	source     = `(?i)hdtv|bluray|(?:b[dr]|dvd|hd|tv|webhd|web)rip|web-?(?:dl|rip|hd)|dvd(scr)?|bd|ituneshd|itunes|\b(?:hd)?ts\b|amazon(?:hd)?|netflix(?:hd)?|[ad]TV`
-	codec      = `(?i)dvix|mpeg[0-9]|divx|xvid|(?:x|h)[-\. ]?26(?:4|5)|avc|hevc|vp(?:8|9)`
-	audio      = `(?i)MP3|FLAC|DD[\s\.]?5\.?1|Dual[\- ]Audio|LiNE|DTS|AAC(?:\.?2\.0)?|AC3(?:\.5\.1)?`
+	resolution = `(?P<480p>480p|640x480|848x480)|(?P<576p>576p)|(?P<720p>720p|1280x720)|(?P<1080p>1080p|1920x1080)|(?P<2160p>2160p)`
+	source     = `(?i)(\b)(?P<bdrip>BDRip)|(?P<brrip>BRRip)|(?P<bluray>BluRay|Blu-Ray|HDDVD|BD)|(?P<webdl>WEB[-_. ]DL|HDRIP|WEBDL|FUNi-DL|WebRip|Web-Rip|AmazonHD|NetflixHD|iTunesHD|WebHD|[. ]WEB[. ](?:[xh]26[45]|DD5[. ]1)|\\d+0p[. ]WEB[. ])|(?P<hdtv>HDTV)|(?P<scr>SCR|SCREENER|DVDSCR|DVDSCREENER)|(?P<dvdr>DVD-R|DVDRip|DVDR?|NTSC|PAL|xvidvd)|(?P<dsr>WS[-_. ]DSR|DSR)|(?P<ts>\bTS|TELESYNC|HD-TS|HDTS|PDVD\b)|(?P<tc>TC|TELECINE|HD-TC|HDTC)|(?P<cam>CAMRIP|CAM|HDCAM|HD-CAM)|(?P<wp>WORKPRINT|WP)|(?P<pdtv>PDTV)|(?P<sdtv>SDTV)|(?P<tvrip>TVRip|[ad]TV)(\b)`
+	// codec      = `(?i)dvix|mpeg[0-9]|divx|xvid(?:hd)?|(?:x|h)[-\. ]?26(?:4|5)|avc|hevc|vp(?:8|9)`
+	codec      = `(?i)(?P<x264>x264)|(?P<h264>h264)|(?P<h265>h265|hevc)|(?P<xvidhd>XvidHD)|(?P<xvid>X-?vid)|(?P<divx>divx|mpeg[0-9])(?P<vp>vp(?:8|9))`
+	audio      = `(?i)MP3|FLAC|DD[\s\.]?5\.?1|Dual[\- ]Audio|LiNE|DTS|AAC(?:\.?2\.0)?|AC3D?(?:\.5\.1)?`
 	group      = `(?:- ?([^-]+))$`
-	region     = `R[0-9]`
+	region     = `R[0-9]{1}`
 	doku       = `(?i)\bDOKU\b`
-	extended   = `EXTENDED`
-	uncut      = `UNCUT`
-	hardcoded  = `HC`
-	proper     = `PROPER`
+	extended   = `(?i)\bEXTENDED\b`
+	uncut      = `(?i)\bUNCUT\b`
+	hardcoded  = `(?i)\bHC\b`
+	proper     = `(?i)\bPROPER\b`
 	subbed     = `(?i)subbed|ger[.-]?sub(s|ed)?|nlsub|eng-sub`
-	repack     = `REPACK`
+	repack     = `(?i)\bREPACK\b`
 	is3d       = `(?i)\b3d\b`
-	widescreen = `WS`
-	container  = `MKV|AVI|MP4|mkv|avi|mp4`
+	widescreen = `(?i)\bWS\b`
+	container  = `(?i)\bMKV|AVI|MP4|mkv|avi|mp4\b`
 	website    = `^(\[ ?([^\]]+?) ?\])`
 	language   = `(?i)\b(?:TRUE)?FR(?:ENCH)?\b|\bDE(?:UTSCH)?\b|\bGERMAN\b|\bEN(?:G(?:LISH)?)?\b|\bVOST(?:(F(?:R)?)|A)?\b|\bMULTI(?:Lang|Truefrench|\-VF2)?\b|\bSUBFRENCH\b|\bHindi\b`
 )
 
 // Release represents a scene release
 type Release struct {
-	Input      string
-	Title      string
-	Type       string
-	Season     int
-	Episode    int
-	Year       int
-	Resolution string
-	Source     string
-	Codec      string
-	Audio      string
-	Group      string
-	Region     string
-	Doku       bool
-	Extended   bool
-	Hardcoded  bool
-	Subbed     bool
-	Proper     bool
-	Repack     bool
-	Is3D       bool
-	Uncut      bool
-	Container  string
-	Website    string
-	Widescreen bool
-	Language   string
-	start      int
-	end        int
-	parts      map[string]string
+	Input       string
+	Title       string
+	Type        string
+	Season      int
+	Episode     int
+	Year        int
+	Resolution  string
+	Source      string
+	SourceGroup string
+	Codec       string
+	CodecGroup  string
+	Audio       string
+	AudioGroup  string
+	Group       string
+	Region      string
+	Container   string
+	Website     string
+	Language    string
+	Doku        bool
+	Extended    bool
+	Hardcoded   bool
+	Subbed      bool
+	Proper      bool
+	Repack      bool
+	Is3D        bool
+	Uncut       bool
+	Widescreen  bool
+	start       int
+	end         int
+	parts       map[string]string
 }
 
 func parseInt(s string) int {
@@ -83,6 +87,19 @@ func (r *Release) part(name string, match string, clean string) {
 			r.end = index
 		}
 	}
+}
+
+func getMatchedGroupName(re *regexp.Regexp, s string) string {
+	match := re.FindStringSubmatch(s)
+
+	for i, name := range re.SubexpNames() {
+		if i > 0 && i <= len(match) {
+			if match[i] != "" {
+				return strings.ToUpper(name)
+			}
+		}
+	}
+	return ""
 }
 
 func cleanTitle(name string) string {
@@ -117,12 +134,13 @@ func Parse(s string) *Release {
 	}
 	re = regexp.MustCompile(resolution)
 	if loc := re.FindStringIndex(s); loc != nil {
-		r.Resolution = s[loc[0] : loc[1]-1]
+		r.Resolution = strings.ToLower(getMatchedGroupName(re, s[loc[0]:loc[1]]))
 		r.part("resolution", r.Input, s[loc[0]:loc[1]])
 	}
 	re = regexp.MustCompile(source)
 	if loc := re.FindStringIndex(s); loc != nil {
 		r.Source = s[loc[0]:loc[1]]
+		r.SourceGroup = getMatchedGroupName(re, s[loc[0]:loc[1]])
 		r.part("source", r.Input, s[loc[0]:loc[1]])
 	}
 	re = regexp.MustCompile(language)
@@ -133,11 +151,13 @@ func Parse(s string) *Release {
 	re = regexp.MustCompile(codec)
 	if loc := re.FindStringIndex(s); loc != nil {
 		r.Codec = s[loc[0]:loc[1]]
+		r.CodecGroup = getMatchedGroupName(re, s[loc[0]:loc[1]])
 		r.part("codec", r.Input, s[loc[0]:loc[1]])
 	}
 	re = regexp.MustCompile(audio)
 	if loc := re.FindStringIndex(s); loc != nil {
 		r.Audio = s[loc[0]:loc[1]]
+		r.AudioGroup = getMatchedGroupName(re, s[loc[0]:loc[1]])
 		r.part("audio", r.Input, s[loc[0]:loc[1]])
 	}
 	re = regexp.MustCompile(website)
